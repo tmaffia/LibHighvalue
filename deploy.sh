9fb0ac75f23c2719b1847f3191c730651303e93e
Tom Maffia <ta.maffia@gmail.com>
2020-12-11 02:30:01 -0600

    Add curseforge packaging option to deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,11 @@ elif [ $1 == 'classic' ]
 then
     echo 'Deploying addon to classic wow'
     cp -r ./LibHighvalue "$classic_addons"
+elif [[ -n $2 && $1 == 'package'  ]]
+then
+    echo "Packaging version $2 for distrobution"
+    zip -r ./LibHighvalue-$2.zip LibHighvalue
 else 
     echo 'Invalid cmd line argument!'
-    echo 'Options: classic | retail'
+    echo 'Options: classic | retail | package'
 fi
